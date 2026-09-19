e27532033b55a2fa079a8b46f45acb381cc7bcef
Jiyakumari <jiyapradeepyadav@gmail.com>
2026-09-19 21:53:35 +0530

    Add Terraform AWS S3 configuration

---
+++ main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-north-1"
+}
+
+resource "aws_s3_bucket" "lab_bucket" {
+  bucket = "t-aws-lab-jiy208-2026"
+}
