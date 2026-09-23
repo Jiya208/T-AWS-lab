88db0d4e8f491522fd26e3f0ea65f39ecc09584e
Jiyakumari <jiyapradeepyadav@gmail.com>
2026-09-23 11:23:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,5 +12,40 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "lab_bucket" {
-  bucket = "t-aws-lab-jiy208-2026"
+  bucket = "t-aws-lab-jiya208-2026"
+}
+
+resource "aws_s3_bucket_policy" "lab_bucket_policy" {
+  bucket = aws_s3_bucket.lab_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "HTTPSOnly"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = "s3:*"
+        Resource = [
+          aws_s3_bucket.lab_bucket.arn,
+          "${aws_s3_bucket.lab_bucket.arn}/*"
+        ]
+        Condition = {
+          Bool = {
+            "aws:SecureTransport" = "false"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_s3_bucket" "log_bucket" {
+  bucket = "t-aws-lab-jiya208-2026-logs"
+}
+
+resource "aws_s3_bucket_logging" "lab_bucket_logging" {
+  bucket        = aws_s3_bucket.lab_bucket.id
+  target_bucket = aws_s3_bucket.log_bucket.id
+  target_prefix = "log/"
 }
