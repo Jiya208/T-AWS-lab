80b1b737b0db6203fc57ff51b3cd97b434688dcb
Jiyakumari <jiyapradeepyadav@gmail.com>
2026-09-23 15:23:33 +0530

    Add HTTPS policy for log bucket

--- main.tf
+++ main.tf
@@ -50,3 +50,26 @@ resource "aws_s3_bucket_logging" "lab_bucket_logging" {
   target_prefix = "log/"
 }
 # SonarQube re-scan
+resource "aws_s3_bucket_policy" "log_bucket_policy" {
+  bucket = aws_s3_bucket.log_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = "s3:*"
+        Resource = [
+          aws_s3_bucket.log_bucket.arn,
+          "${aws_s3_bucket.log_bucket.arn}/*"
+        ]
+        Condition = {
+          Bool = {
+            "aws:SecureTransport" = "false"
+          }
+        }
+      }
+    ]
+  })
+}
